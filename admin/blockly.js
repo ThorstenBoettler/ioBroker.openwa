9bb1c628bbe1979b28feb608f57c24ab0b678599
Thorsten Böttler <thorsten.boettler@freenet.de>
2026-05-23 13:52:48 +0200

    Adding Output of API-Response as Result

--- admin/blockly.js
+++ admin/blockly.js
@@ -7,6 +7,7 @@ Blockly.Words['openwa_message']     = {'en': 'message',                  'de': '
 Blockly.Words['openwa_phone']       = {'en': 'Recipient (phone number)', 'de': 'Empfänger (Telefonnummer)'};
 Blockly.Words['openwa_anyInstance'] = {'en': 'all instances',            'de': 'Alle Instanzen'};
 Blockly.Words['openwa_tooltip']     = {'en': 'Send WhatsApp via Open-WA', 'de': 'Sende eine WhatsApp-Nachricht über Open-WA'};
+Blockly.Words['openwa_response']    = {'en': 'with result in',           'de': 'Ergebnis in'};
 
 Blockly.Sendto.blocks['openwa'] =
     '<block type="openwa">'
@@ -57,6 +58,11 @@ Blockly.Blocks['openwa'] = {
             .setCheck('String')
             .appendField(Blockly.Words['openwa_phone'][systemLang]);
 
+        this.appendStatementInput('ON_RESPONSE')
+            .setCheck(null)
+            .appendField(Blockly.Words['openwa_response'][systemLang])
+            .appendField(new Blockly.FieldVariable('result'), 'VAR');
+
         this.setInputsInline(false);
         this.setPreviousStatement(true, null);
         this.setNextStatement(true, null);
@@ -70,6 +76,29 @@ Blockly.JavaScript['openwa'] = function(block) {
     var value_message = Blockly.JavaScript.valueToCode(block, 'MESSAGE', Blockly.JavaScript.ORDER_ATOMIC) || "''";
     var value_phone = Blockly.JavaScript.valueToCode(block, 'PHONE', Blockly.JavaScript.ORDER_ATOMIC) || "''";
 
+    var variable_name = 'result';
+    try {
+        var varId = block.getFieldValue('VAR');
+        if (block.workspace && block.workspace.getVariableById) {
+            var variable = block.workspace.getVariableById(varId);
+            if (variable) {
+                variable_name = variable.name;
+            }
+        } else if (Blockly.JavaScript.variableDB_) {
+            variable_name = Blockly.JavaScript.variableDB_.getName(varId, Blockly.Variables.NAME_TYPE);
+        }
+    } catch (e) {
+        variable_name = 'result';
+    }
+
+    var branch = Blockly.JavaScript.statementToCode(block, 'ON_RESPONSE');
+    branch = Blockly.JavaScript.addLoopTrap(branch, block.id);
+
     var adapterInstance = 'openwa' + (dropdown_instance ? dropdown_instance : '.0');
-    return "sendTo('" + adapterInstance + "', 'send', { to: " + value_phone + ", text: " + value_message + " });\n";
+
+    if (branch) {
+        return "sendTo('" + adapterInstance + "', 'send', { to: " + value_phone + ", text: " + value_message + " }, async function (" + variable_name + ") {\n" + branch + "});\n";
+    } else {
+        return "sendTo('" + adapterInstance + "', 'send', { to: " + value_phone + ", text: " + value_message + " });\n";
+    }
 };
